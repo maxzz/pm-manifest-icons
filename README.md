6af74d81872495362b39eeff199d06923af98a39
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-08 14:45:27 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#### About
+
+PM manifest icon files.
+
+These definitions are used in the following projects:
+* [pmit](https://github.com/maxzz/dropzone)
+* [electron-window-monitor](https://github.com/maxzz/electron-window-monitor)
+
+#### Links, credits, refs
+
+Not now.
---
+++ src/symbols/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { DefFieldTypes } from './field';
+
+export * from './field';
+
+export function UISymbolDefs() {
+    return (
+        <svg id="svgfont" aria-hidden="true" style={{ position: 'absolute', width: 0, height: 0, overflow: 'hidden' }}
+            xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1"
+        >
+            <defs>
+                {DefFieldTypes()}
+            </defs>
+        </svg>
+    );
+}
